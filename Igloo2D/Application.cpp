f99d0c995d22c60c321e2de3978cdcb92291fb83
James Karibian <jjk6895@psu.edu>
2025-12-11 22:20:33 -0500

    Added render target

--- Igloo2D/Application.cpp
+++ Igloo2D/Application.cpp
@@ -11,6 +11,16 @@ Application::Application(HINSTANCE hInstance)
 
 	hwnd = CreateWindowEx(0, wc.lpszClassName, L"Igloo 2D", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 800, 600, NULL, NULL, hInstance, nullptr);
 	ShowWindow(hwnd, SW_SHOWNORMAL);
+
+	HRESULT hr = D2D1CreateFactory(D2D1_FACTORY_TYPE_SINGLE_THREADED, d2dFactory.put());
+	
+	RECT rc;
+	GetClientRect(hwnd, &rc);
+	hr = d2dFactory->CreateHwndRenderTarget(D2D1::RenderTargetProperties(), 
+		D2D1::HwndRenderTargetProperties(hwnd, D2D1::SizeU(rc.right - rc.left, rc.bottom - rc.top)), 
+		renderTarget.put());
+
+	hr = renderTarget->CreateSolidColorBrush(D2D1::ColorF(255.0f, 255.0f, 0.0f), brush.put());
 }
 
 Application::~Application()
@@ -35,7 +45,11 @@ void Application::Start()
 		else
 		{
 			// Do game shit
-			
+			renderTarget->BeginDraw();
+
+			renderTarget->FillRectangle(D2D1::RectF(100.0f, 100.0f, 200.0f, 200.0f), brush.get());
+
+			renderTarget->EndDraw();
 		}
 
 	}
--- Igloo2D/Application.h
+++ Igloo2D/Application.h
@@ -1,6 +1,10 @@
 #pragma once
 
 #include <Windows.h>
+#include <d2d1.h>
+#include <winrt/base.h>
+
+using winrt::com_ptr;
 
 class Application
 {
@@ -10,7 +14,10 @@ public:
 	void Start();
 	bool IsKeyDown(int keyCode);
 private:
-	HWND hwnd;
 	static LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+	HWND hwnd;
+	com_ptr<ID2D1Factory> d2dFactory;
+	com_ptr<ID2D1HwndRenderTarget> renderTarget;
+	com_ptr<ID2D1SolidColorBrush> brush;
 };
 
